639701cdc09e374c81795df1aa9b2202b9057e35
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-07-10 20:05:20 +0600

    server initiated

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+// Middleware
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Mentor Finder API is running...')
+    }
+)
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
